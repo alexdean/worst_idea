5b267dd1d10c97e1faea6a77507dc124300a421a
Alex Dean <github@mostlyalex.com>
2019-09-28 17:16:18 -0500

    js skeleton will be for player app (what normal clients load to play the game.)

--- app/javascript/packs/player.jsx
+++ app/javascript/packs/player.jsx
@@ -1,4 +1,4 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// Run this example by adding <%= javascript_pack_tag 'player' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
@@ -25,22 +25,22 @@ bugsnagClient.use(bugsnagReact, React)
 // wrap your entire app tree in the ErrorBoundary provided
 const ErrorBoundary = bugsnagClient.getPlugin('react')
 
-const Hello = props => (
+const App = props => (
   <div>Hello {props.name}!</div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
+App.defaultProps = {
+  name: 'Player'
 }
 
-Hello.propTypes = {
+App.propTypes = {
   name: PropTypes.string
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <ErrorBoundary>
-      <Hello name="React" />
+      <App name="React" />
     </ErrorBoundary>,
     document.body.appendChild(document.createElement('div')),
   )
