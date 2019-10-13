402bd79928277758cca8756e53de719873acb0a6
Alex Dean <github@mostlyalex.com>
2019-10-13 12:56:47 -0500

    move GameState class to its own file

---
+++ lib/game_state.rb
@@ -0,0 +1,50 @@
+require 'set'
+
+# value object for assigning new data to a game document
+#
+# should raise when invalid keys or values are assigned.
+class GameState
+  VALID_KEYS = Set.new([
+    :title,
+    :current_stage,
+    :leader_player_id,
+    :active_question_id,
+    :active_question_max_answer_id,
+    :created_at,
+    :summary
+  ])
+
+  # game states
+  JOINING = 'joining'
+  QUESTION_OPEN = 'question-open'
+  QUESTION_CLOSED = 'question-closed'
+  QUESTION_RESULTS = 'question-results'
+  FINISHED = 'finished'
+
+  VALID_STAGES = Set.new([
+    JOINING,
+    QUESTION_OPEN,
+    QUESTION_CLOSED,
+    QUESTION_RESULTS,
+    FINISHED
+  ])
+
+  def initialize(data = {})
+    @data = {}
+    data.each do |key, value|
+      set(key, value)
+    end
+  end
+
+  def set(key, val)
+    raise ArgumentError, "#{key} is not a valid key." if !VALID_KEYS.include?(key)
+    if key == :current_stage && !VALID_STAGES.include?(val)
+      raise ArgumentError, "#{val} is not a valid value for #{key}."
+    end
+    @data[key] = val
+  end
+
+  def to_h
+    @data
+  end
+end
