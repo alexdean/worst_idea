5bce8678ecd72c46212e1502601e5bbce3d34fd9
Aaron Weyenberg <aaron@ted.com>
2019-10-13 16:48:45 -0400

    Better waiting room experience.

--- app/javascript/components/Player.js
+++ app/javascript/components/Player.js
@@ -27,11 +27,7 @@ const Player = () => {
   const [user, initializing, error] = useAuthState(firebase.auth());
 
   // Get and store questions for the gameId stored in the above state...
-<<<<<<< HEAD
-  const [questionsValue, questionsLoading, questionsError] = useCollectionOnce(
-=======
   const [questionsValue, questionsLoading, questionsError] = useCollection(
->>>>>>> player-view
     gameId &&
       user &&
       db
@@ -60,35 +56,6 @@ const Player = () => {
   const [gamesValue, gamesLoading, gamesError] = useCollectionOnce(
     db.collection("games").where("current_stage", "==", "joining")
   );
-<<<<<<< HEAD
-
-  const logout = () => {
-    firebase.auth().signOut();
-    Cookies.remove("_worst_idea_game_id");
-  };
-
-  const login = game => {
-    console.log("Authenticating...");
-    // todo: validate name input
-    firebase
-      .auth()
-      .signInAnonymously()
-      .catch(error => {
-        console.error("Firebase auth error", error);
-      })
-      .then(u => {
-        let gameId = game.data().title;
-        setGameId(gameId);
-        Cookies.set("_worst_idea_game_id", gameId);
-        return db
-          .collection("games")
-          .doc(gameId)
-          .collection("players")
-          .doc(u.user.uid)
-          .set({ is_active: true, name: nameRef.current.value });
-      });
-  };
-=======
 
   const logout = () => {
     let c = confirm("You can't rejoin after the game starts. Really quit?");
@@ -148,35 +115,6 @@ const Player = () => {
       }
     }
   }, [gameValue]);
->>>>>>> player-view
-
-  useEffect(() => {
-<<<<<<< HEAD
-    let c = Cookies.get("_worst_idea_game_id");
-    console.log("Game id cookie: ", c);
-    c && setGameId(c);
-  }, [user]);
-
-  useEffect(() => {
-    console.log("Player value: ", playerValue);
-    playerValue && setPlayerIsActive(playerValue.data().is_active);
-  }, [playerValue]);
-
-  // Runs whenever the game document updates
-  useEffect(() => {
-    if (gameValue) {
-      // Update the current question id
-      let qid = currentQuestionId;
-      console.log("question id: ", gameValue.data().active_question_id);
-      setCurrentQuestionId(gameValue.data().active_question_id);
-      setStage(gameValue.data().current_stage);
-      setLeaderId(gameValue.data().leader_player_id);
-      // If the current question changed, then reset the player's selected answer
-      if (qid != gameValue.data().active_question_id) {
-        setSelectedAnswer(null);
-      }
-    }
-  }, [gameValue]);
 
   useEffect(() => {
     gamesValue &&
@@ -185,28 +123,15 @@ const Player = () => {
       });
   }, [gamesValue]);
 
-=======
-    gamesValue &&
-      gamesValue.docs.map((game, i) => {
-        // console.log(game.data().title);
-      });
-  }, [gamesValue]);
-
->>>>>>> player-view
   // Runs when the questions collection is updated
   useEffect(() => {
     questionsValue &&
       setQuestions(questionsValue.docs.map((question, i) => question.data()));
   }, [questionsValue]);
 
-<<<<<<< HEAD
-  const onAnswerSelect = e => {
-    let answerIndex = parseInt(e.target.attributes["data-value"].value);
-=======
   const onAnswerSelect = (e, i) => {
     // let answerIndex = parseInt(e.target.attributes["data-value"].value);
     let answerIndex = i;
->>>>>>> player-view
     console.log("Clicked: ", answerIndex, user.uid);
     if (stage === "question-open") {
       setSelectedAnswer(answerIndex);
@@ -224,55 +149,6 @@ const Player = () => {
   // If auth'd...
   if (user) {
     return (
-<<<<<<< HEAD
-      <div className="p-4 pt-10 text-gray-200">
-        <div className="p-2">
-          {questions.length > 0 && currentQuestionId !== null && (
-            <div className="">
-              <div className="text-3xl font-bold leading-tight mb-6 text-center">
-                {questions[currentQuestionId].question}
-              </div>
-              <div className="">
-                {questions[currentQuestionId].answers.map((answer, i) => {
-                  return (
-                    <div className="my-4" key={i}>
-                      <div
-                        key={i}
-                        onClick={e => onAnswerSelect(e)}
-                        data-value={i}
-                        className={`answer rounded-full p-3 inline-block w-full text-center ${
-                          selectedAnswer == i ? "bg-indigo-700" : "bg-gray-800"
-                        }`}
-                      >
-                        {answer}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="text-center">
-                {stage === "question-closed" && (
-                  <div className="">Answers are now locked in</div>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
-
-        <div className="bottom-0 inset-x-0 fixed border-t border-gray-800 p-4">
-          <div className="flex items-center justify-between">
-            <div className="">
-              {playerValue && (
-                <div className="">
-                  {playerValue.data().name} is{" "}
-                  {playerIsActive ? "in" : "out of"} the game.
-                </div>
-              )}
-            </div>
-            <div className="">
-              <button className="text-indigo-500" onClick={logout}>
-                Log out
-=======
       <div className="text-gray-200">
         <div className="">
           {stage === "joining" && (
@@ -350,7 +226,6 @@ const Player = () => {
             <div className="font-bold">
               <button className="opacity-50" onClick={logout}>
                 Quit
->>>>>>> player-view
               </button>
             </div>
           </div>
@@ -373,16 +248,6 @@ const Player = () => {
         </div>
       )}
       <div className="my-5">
-<<<<<<< HEAD
-        <div className="font-bold mb-4 py-1 text-xl">Who are you?</div>
-        <input
-          className="border-0 bg-gray-900 p-2 w-full text-lg"
-          placeholder="Your name"
-          ref={nameRef}
-        />
-      </div>
-      <div className="font-bold mb-4 py-1 text-xl">Choose a game to join</div>
-=======
         <div className="mb-4">
           <div className="font-bold text-2xl">Who are you?</div>
           <div className="text-sm text-gray-600">Max 30 chars</div>
@@ -395,7 +260,6 @@ const Player = () => {
         />
       </div>
       <div className="font-bold mb-4 py-1 text-2xl">Choose a game to join</div>
->>>>>>> player-view
       <div className="">
         {gamesValue &&
           gamesValue.docs.map((game, i) => {
@@ -406,11 +270,7 @@ const Player = () => {
               >
                 <div className="">{game.data().title}</div>
                 <button
-<<<<<<< HEAD
-                  className="bg-indigo-600 text-white p-2 px-4 rounded"
-=======
                   className="bg-indigo-600 text-white p-2 px-6 rounded"
->>>>>>> player-view
                   onClick={() => login(game)}
                 >
                   Join
