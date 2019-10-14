95b60efd218cb6ab8cfab05c231ffad8e4da75e8
Aaron Weyenberg <aaron@ted.com>
2019-10-13 22:22:39 -0400

    Show percentages of answers.
    todo: make sure the total count is capped by the max answer id.

--- app/javascript/components/Player.js
+++ app/javascript/components/Player.js
@@ -19,6 +19,7 @@ const Player = () => {
   const [currentQuestionId, setCurrentQuestionId] = useState(null); // stores current question index
   const [selectedAnswer, setSelectedAnswer] = useState(null); // holds player's currently selected answer index
   const [playerIsActive, setPlayerIsActive] = useState(false); // store player active state. False after an incorrect answer
+  const [summary, setSummary] = useState({ totalAnswerCount: 0 }); // store the current question's summary
 
   // App refs
   const nameRef = useRef(null); // reference to the name input
@@ -87,21 +88,25 @@ const Player = () => {
       });
   };
 
-  // useEffect runs whenever the values listed in the last argument change
+  // useEffect runs whenever the values listed in their last argument (the array) change.
   // In this case, whenever {user} updates
   useEffect(() => {
+    console.log("user updated: ", user);
     let c = Cookies.get("_worst_idea_game_id");
     console.log("Game id cookie: ", c);
     c && setGameId(c);
   }, [user]);
 
+  // Runs when the player doc updates
   useEffect(() => {
-    console.log("Player value: ", playerValue);
+    console.log("playerValue updated: ", playerValue);
     playerValue && setPlayerIsActive(playerValue.data().is_active);
-  }, [playerValue]);
+    playerError && console.error("Error updating player", playerError);
+  }, [playerValue, playerError]);
 
-  // Runs whenever the game document updates
+  // Runs whenever the game doc updates
   useEffect(() => {
+    console.log("gameValue updated: ", gameValue);
     if (gameValue) {
       // Update the current question id
       let qid = currentQuestionId;
@@ -113,10 +118,13 @@ const Player = () => {
       if (qid != gameValue.data().active_question_id) {
         setSelectedAnswer(null);
       }
+      summarize(gameValue.data().summary);
     }
   }, [gameValue]);
 
+  // Runs when the root collection of games is fetched
   useEffect(() => {
+    console.log("gamesValue udpated: ", gamesValue);
     gamesValue &&
       gamesValue.docs.map((game, i) => {
         // console.log(game.data().title);
@@ -129,6 +137,7 @@ const Player = () => {
       setQuestions(questionsValue.docs.map((question, i) => question.data()));
   }, [questionsValue]);
 
+  // Respond to clisk on answer options
   const onAnswerSelect = (e, i) => {
     // let answerIndex = parseInt(e.target.attributes["data-value"].value);
     let answerIndex = i;
@@ -146,34 +155,59 @@ const Player = () => {
     }
   };
 
+  // Calculate the total answer count for the active question
+  const summarize = data => {
+    const reducer = (accumulator, current) => accumulator + current;
+    let totalAnswerCount = Object.values(data).reduce(reducer);
+    setSummary({
+      total: totalAnswerCount
+    });
+  };
+
+  // Renders ––––––––––––––––––––
+
   // If auth'd...
   if (user) {
     return (
       <div className="text-gray-200">
         <div className="">
           {stage === "joining" && (
-            <div className="text-center">
+            <div className="">
               <div
-                className="inset-0 absolute opacity-75"
+                className="inset-0 absolute opacity-50"
                 style={{
                   backgroundSize: "cover",
                   backgroundRepeat: "no-repeat",
                   backgroundPosition: "50% 50%",
                   backgroundImage:
-                    "url(https://media.giphy.com/media/l4Epl2MzXZW4mF15K/giphy-downsized.gif)"
+                    "url(https://media.giphy.com/media/tXL4FHPSnVJ0A/giphy.gif)"
                 }}
               />
               <div className="absolute inset-0 flex items-center justify-center content-center text-white">
-                <div className="">
+                <div className="text-center">
                   <div className="text-3xl font-bold px-8 leading-tight">
-                    Waiting for the game to begin.
+                    Waiting for players.
                   </div>
-                  <div className="my-5">Other players are joining.</div>
+                  <div className="my-5">This shouldn't take long.</div>
+                </div>
+              </div>
+            </div>
+          )}
+          {stage === "preparing" && (
+            <div className="absolute inset-0 flex items-center justify-center content-center text-white">
+              <div className="text-center">
+                <div className="text-3xl font-bold px-8 leading-tight">
+                  Ok {playerValue && <span>{playerValue.data().name}</span>},
+                  we're about ready to start.
+                </div>
+                <div className="my-5">
+                  Just doing a bit of pregame housekeeping.
                 </div>
               </div>
             </div>
           )}
           {stage !== "joining" &&
+            stage !== "preparing" &&
             questions.length > 0 &&
             currentQuestionId !== null && (
               <div className="p-8">
@@ -182,11 +216,16 @@ const Player = () => {
                 </div>
                 <div className="">
                   {questions[currentQuestionId].answers.map((answer, i) => {
+                    let count = gameValue.data().summary[i]
+                      ? gameValue.data().summary[i]
+                      : 0;
+                    let percentage = Math.floor((count / summary.total) * 100);
+                    console.log("count", count);
+                    console.log(summary.total);
                     return (
                       <div className="my-4" key={i}>
                         <div
                           onClick={e => onAnswerSelect(e, i)}
-                          // data-value={i}
                           className={`answer text-lg rounded-full inline-block w-full overflow-hidden text-center ${
                             selectedAnswer == i ? "bg-red-600" : "bg-gray-800"
                           }`}
@@ -194,9 +233,7 @@ const Player = () => {
                           <div
                             className="p-3"
                             style={{
-                              // mixBlendMode: "multiply",
-                              backgroundImage:
-                                "linear-gradient(90deg, rgba(246,246,246,.2) 0%, rgba(246,246,246,.2) 55%, rgba(246,246,246,0) 55%)"
+                              backgroundImage: `linear-gradient(90deg, rgba(246,246,246,.2) 0%, rgba(246,246,246,.2) ${percentage}%, rgba(246,246,246,0) ${percentage}%)`
                             }}
                           >
                             {answer}
@@ -220,8 +257,15 @@ const Player = () => {
 
         <div className="bottom-0 inset-x-0 fixed">
           <div className="flex items-center justify-between p-4">
-            <div className="font-bold">
-              {playerValue && <div className="">{playerValue.data().name}</div>}
+            <div className="">
+              {playerValue && (
+                <div className="">
+                  <div className="font-bold">{playerValue.data().name}</div>
+                  <div className="text-sm">
+                    {!playerIsActive && "Damn. You're out of the game 😕"}
+                  </div>
+                </div>
+              )}
             </div>
             <div className="font-bold">
               <button className="opacity-50" onClick={logout}>
@@ -234,7 +278,8 @@ const Player = () => {
     );
   }
 
-  // If user not defined, list available games
+  // If user not defined, list available games. This the entrance to the player app.
+  // All players will see this at the beginning of their experience.
   return (
     <div className="p-4 text-gray-200">
       {initializing && (
@@ -256,7 +301,7 @@ const Player = () => {
           className="border-0 bg-gray-800 p-2 w-full text-lg"
           placeholder="Your name"
           ref={nameRef}
-          maxlength={30}
+          maxLength={30}
         />
       </div>
       <div className="font-bold mb-4 py-1 text-2xl">Choose a game to join</div>
